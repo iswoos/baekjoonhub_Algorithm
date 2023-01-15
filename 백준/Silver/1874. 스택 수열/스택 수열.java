1f182ee43e3f61275a012126a866d896f6a6c7fc
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-01-15 15:31:19 +0900

    [Silver II] Title: 스택 수열, Time: 1020 ms, Memory: 111472 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+
+        int num = 1;
+        boolean result = true;
+
+        for (int i = 0; i < arr.length; i++) {
+            int su = arr[i];
+            if (su >= num) {
+                while (su >= num) {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            }
+
+            else {
+                int q = stack.pop();
+                if (q > su) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    bf.append("-\n");
+                }
+            }
+        }
+        if (result) {
+            System.out.println(bf.toString());
+        }
+    }
+}
