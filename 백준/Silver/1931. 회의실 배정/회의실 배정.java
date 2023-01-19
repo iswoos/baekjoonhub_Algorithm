b07df609f2c91ee246b733b02c5746698bf8502b
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-01-19 16:13:19 +0900

    [Silver I] Title: 회의실 배정, Time: 1232 ms, Memory: 181156 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 1. N에따라 개수 지정
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[][] arr = new int[N][2];
+
+        // 2. N개수에 따라 값 기입
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = scan.nextInt();
+            arr[i][1] = scan.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int end = -1;
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i][0] >= end) {
+                end = arr[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
