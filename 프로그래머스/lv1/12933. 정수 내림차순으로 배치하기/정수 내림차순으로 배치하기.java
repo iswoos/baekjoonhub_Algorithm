d88b260df091cd59b56bd3bc33b7990980f3170e
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-01-26 22:53:17 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 10.81 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -4,22 +4,19 @@ class Solution {
     public long solution(long n) {
         
         String num = String.valueOf(n);
-        
-        String ansnum = "";
+        String strAnswer = "";
         
         char [] arr = new char[num.length()];
-        
         for (int i = 0; i < num.length(); i++) {
         	arr[i] += num.charAt(i);
         }
         
         Arrays.sort(arr);
-        
         for (int i = arr.length-1; i >= 0; i--) {
-        	ansnum += arr[i];
+        	strAnswer += arr[i];
         }
         
-        long answer = Long.parseLong(ansnum);
+        long answer = Long.parseLong(strAnswer);
         return answer;
     }
 }
