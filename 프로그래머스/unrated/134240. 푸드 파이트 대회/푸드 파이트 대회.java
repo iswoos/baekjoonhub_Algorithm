f19b134afd38b771a4e66d6c1d5fb4e7d9649840
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-02-02 18:05:38 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 11.08 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,21 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
+        String answer = "0";
 
-        StringBuilder sb = new StringBuilder();
-
-        // 2명이니까 2명 배분할 값만 할당
-        for (int i = 1; i < food.length; i++) {
-            int foods = food[i] / 2;
-
-            // 배분할 값의 수만큼 int[] food에 있는 값 StringBuilder에 append
-            for (int j = 0; j < foods; j++) {
-                sb.append(i);
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
 
-//        String answer = sb.toString() + "0" + sb.reverse().toString();
-
-        return answer +sb.toString() + "0" + sb.reverse().toString();
+        return answer;
     }
 }
