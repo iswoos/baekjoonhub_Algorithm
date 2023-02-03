b3b5f2aa92cfa8e8d04d414fcb05eabb506c62a6
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-02-03 15:11:58 +0900

    Update 택배 배달과 수거하기.java

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -4,12 +4,18 @@ class Solution {
     // 둘 중 어느 끝이 더 큰지 비교해서 (큰 인덱스+1)*2를 정답에 추가.
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        if(isAllZero(deliveries, pickups)) return 0;
+        
+        // 전부 다 0일 경우를 대비하여 예외처리 메소드 작성
+        if(AllZero(deliveries, pickups)) return 0;
 
+        
+        // 배열 맨 뒤에 0이 아닌 값을 지니고 있는 인덱스로 변수지정
         int deliveryLastIndex = getDeliveryLastIndex(deliveries);
-        int pickupLastIndex = getDeliveryLastIndex(pickups);
-        answer += 2*(Math.max(deliveryLastIndex,pickupLastIndex)+1);
+        int pickupLastIndex = getPickupLastIndex(pickups);
+        answer += 2 * (Math.max(deliveryLastIndex,pickupLastIndex)+1);
 
+        // 변수값 기준으로 조건문 실행
+        // 리턴되는 인덱스를 기준으로 다시 조건문을 실행한다.
         while(deliveryLastIndex >= 0 || pickupLastIndex >= 0) {
             deliveryLastIndex = delivery(deliveries,cap,deliveryLastIndex);
             pickupLastIndex = pickup(pickups,cap,pickupLastIndex);
@@ -36,14 +42,20 @@ class Solution {
         for(int i = deliveryLastIndex; i >= 0; i--) {
             if(cap < 0) {
                 return i;
-            } else if(cap >= deliveries[i]) {
+            } 
+            // 인덱스값보다 cap이 클 경우 cap차감 후 인덱스값 0으로 수정
+            else if(cap >= deliveries[i]) {
                 cap -= deliveries[i];
                 deliveries[i] = 0;
-            } else {
+            } 
+            // 인덱스값이 cap보다 클 경우 인덱스 값에 cap 차감 후 해당 인덱스 리턴
+            else {
                 deliveries[i] -= cap;
                 return i;
             }
         }
+
+        // 전부 아무 이상없이 차감이 진행되었을 경우는 모든 처리가 완료되었음을 의미, -1을 리턴 (아무 곳에도 쓰지않을 숫자임)
         return -1;
     }
 
@@ -62,7 +74,7 @@ class Solution {
         return -1;
     }
 
-    public boolean isAllZero(int[] deliveries, int[] pickups) {
+    public boolean AllZero(int[] deliveries, int[] pickups) {
         return isAllDeliveriesZero(deliveries) && isAllPickupsZero(pickups);
     }
 
