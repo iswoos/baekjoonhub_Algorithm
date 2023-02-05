13e7499751d310086f326f1f446c057535163471
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-02-06 00:58:46 +0900

    [level 2] Title: 가장 큰 수, Time: 1.60 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strArr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strArr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strArr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (strArr[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : strArr) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+}
