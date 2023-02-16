c70e2b22f6d3606d442ab039e65fa781d5bd6b84
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-02-16 12:29:06 +0900

    [unrated] Title: 부대복귀, Time: 102.20 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/unrated/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        // 각 노드마다 인접한 노드들을 저장하는 리스트
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
+        // 양방향 그래프를 만들기 위해 from과 to 둘 다 저장
         for (int[] road : roads) {
             int from = road[0] - 1;
             int to = road[1] - 1;
@@ -13,13 +15,16 @@ class Solution {
             graph.get(to).add(from); // 양방향 그래프
         }
         
+        // 시작점에서부터의 거리 저장
         int[] distance = new int[n];
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[destination - 1] = 0;
         
+        // 우선순위 큐 사용
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         queue.add(destination - 1);
         
+        // 다익스트라 알고리즘 수행
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             for (int next : graph.get(curr)) {
@@ -31,6 +36,7 @@ class Solution {
             }
         }
         
+        // 결과 반환
         int[] answer = new int[sources.length];
         for (int i = 0; i < sources.length; i++) {
             int source = sources[i] - 1;
