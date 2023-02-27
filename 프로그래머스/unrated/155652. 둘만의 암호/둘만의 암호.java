89c575158912b0340b7d586ac480bd50ac423dbc
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-02-27 16:29:49 +0900

    [unrated] Title: 둘만의 암호, Time: 1.19 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,17 +1,29 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s, String skip, int index) {
-        char[] words = s.toCharArray();
-        for(int i = 0 ; i < words.length ; i++){
+        StringBuilder answer = new StringBuilder();
+
+        // char 'a' 의 ASCII 코드는 97 // +1을 하는 등, int계산 가능함
+        // char 'z' 의 ASCII 코드는 122
+
+        // s를 char 배열로 전환
+        char[] charArr = s.toCharArray();
+
+        // char 배열 순회
+        for (int i = 0; i < charArr.length; i++) {
             for(int j = 0 ; j < index ; j++){
+                // 다음 문자로 스킵 선진행
                 do{
-                    words[i]++;
-                    if(words[i] > 'z'){ //범위 초과시 a로 돌아옴
-                        words[i] -= 26; // == 'a'와 동일
+                    charArr[i]++;
+                    if(charArr[i] > 'z'){ //범위 초과시 a로 돌아옴
+                        charArr[i] = 'a';
                     }
-                }while(skip.contains(String.valueOf(words[i])));
+                // 다음 문자가 skip에 존재하는 것이면 j++ 진행 안하고 do문 다시 진행
+                }while(skip.contains(String.valueOf(charArr[i])));
             }
+            answer.append(charArr[i]);
         }
-        String answer = String.valueOf(words);
-        return answer;
+        return answer.toString();
     }
 }
