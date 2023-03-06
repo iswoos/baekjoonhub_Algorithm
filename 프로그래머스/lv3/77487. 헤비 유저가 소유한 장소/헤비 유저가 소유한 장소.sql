ef66f6af85499555a28fc3af27808c0b140464d0
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-03-06 15:55:55 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,10 +1,12 @@
 -- 코드를 입력하세요
 SELECT ID, NAME, HOST_ID
 from PLACES
+# 1. where절 서브쿼리를 이용
+# HOST_ID 값들을 가져올 것이나, 그 조건은 HOST_ID를 그룹화한 뒤 HOST_ID별 개수가 2이상인 HOST_ID 데이터들을 모두 가지고 올 것임
 where (HOST_ID) in(
     select HOST_ID 
     from PLACES
     group by HOST_ID
     having count(HOST_ID) >= 2
 )
-order by ID	
+order by ID
