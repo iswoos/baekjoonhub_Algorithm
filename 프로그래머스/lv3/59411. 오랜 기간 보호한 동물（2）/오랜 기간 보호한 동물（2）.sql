79a516e0e10af91dd736d621e33107fe254391a3
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-03-06 16:05:17 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
 SELECT O.ANIMAL_ID, O.NAME
 from ANIMAL_INS I join ANIMAL_OUTS O on I.ANIMAL_ID = O.ANIMAL_ID
+# datediff 함수 이용하여 종료일, 시작일간 일수계산하여 내림차순으로 정렬 후 2개만 추출
 order by datediff(O.DATETIME,I.DATETIME)+1 desc
 limit 2
