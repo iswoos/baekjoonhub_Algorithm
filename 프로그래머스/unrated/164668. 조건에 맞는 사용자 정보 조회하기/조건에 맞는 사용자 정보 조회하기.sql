ab9cfc97d610a22d4e80613233969669dbf46bb0
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-03-11 13:02:29 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164668. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT U.USER_ID, U.NICKNAME, sum(B.PRICE) as TOTAL_SALES
+from USED_GOODS_BOARD B join USED_GOODS_USER U on B.WRITER_ID = U.USER_ID
+where STATUS = 'DONE'
+group by B.WRITER_ID
+having sum(B.PRICE) >= 700000
+order by TOTAL_SALES 
