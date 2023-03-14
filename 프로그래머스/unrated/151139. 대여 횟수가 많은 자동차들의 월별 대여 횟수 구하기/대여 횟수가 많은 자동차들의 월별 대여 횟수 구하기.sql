8b14c1aa8a5727568c0b495b572f945f7b189816
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-03-14 22:23:56 +0900

    [unrated] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -5,6 +5,11 @@ where CAR_ID in (select CAR_ID
                  where month(START_DATE) >= 8 and month(START_DATE) <=10
                  group by CAR_ID
                  having count(*) >=5
-                ) and month(START_DATE) >= 8 and month(START_DATE) <=10
+                ) 
+# where 조건문을 1개 더 쓴 이유
+# 메인 쿼리에 8월~10월에 대한 where조건문이 없다면
+# count(CAR_ID)의 집계값에 다른 월에 해당하는 케이스도 같이 잡히기 때문이다!!!
+# 이거 때문에 한참 헤맸다...
+        and month(START_DATE) >= 8 and month(START_DATE) <=10
 group by month(START_DATE), CAR_ID
 order by MONTH, CAR_ID desc
