c3aeb7fc240dca740d3dfa6c06b5364d7e3b8b22
Lee seung woo <102216495+iswoos@users.noreply.github.com>
2023-12-12 23:38:35 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(a<=n){
+            answer += n/ a*b;
+            n = n/a*b + n%a;
+        }
+        
+        return answer;
+    }
+}
